3bd172dba0fb8c77d4dde1546b68e0f425d38a0e
Facundo Castro <facu073@gmail.com>
2026-03-18 17:14:27 -0300

    feat: added AuthRepository and impl

--- app/src/main/java/com/facucastro/focusguard/di/AppModule.kt
+++ app/src/main/java/com/facucastro/focusguard/di/AppModule.kt
@@ -4,12 +4,16 @@ import com.facucastro.focusguard.data.local.LocalSessionDataSource
 import com.facucastro.focusguard.data.local.RoomSessionDataSource
 import com.facucastro.focusguard.data.remote.FakeFocusApiServiceImpl
 import com.facucastro.focusguard.data.remote.FocusApiService
+import com.facucastro.focusguard.data.repository.AuthRepositoryImpl
 import com.facucastro.focusguard.data.repository.FocusRepositoryImpl
 import com.facucastro.focusguard.data.time.SystemTimeProvider
+import com.facucastro.focusguard.domain.repository.AuthRepository
 import com.facucastro.focusguard.domain.repository.FocusRepository
 import com.facucastro.focusguard.domain.time.TimeProvider
+import com.google.firebase.auth.FirebaseAuth
 import dagger.Binds
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
@@ -34,4 +38,14 @@ abstract class AppModule {
     @Binds
     @Singleton
     abstract fun bindTimeProvider(impl: SystemTimeProvider): TimeProvider
+
+    @Binds
+    @Singleton
+    abstract fun bindAuthRepository(impl: AuthRepositoryImpl): AuthRepository
+
+    companion object {
+        @Provides
+        @Singleton
+        fun provideFirebaseAuth(): FirebaseAuth = FirebaseAuth.getInstance()
+    }
 }
