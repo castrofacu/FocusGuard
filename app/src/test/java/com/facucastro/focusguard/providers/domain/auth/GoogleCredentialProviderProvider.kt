c24fd9c44187b91df4e024c1860e5d5497a5d7c6
Facundo Castro <facu073@gmail.com>
2026-03-18 20:16:21 -0300

    feat: add mock providers and unit tests for authentication use cases

---
+++ app/src/test/java/com/facucastro/focusguard/providers/domain/auth/GoogleCredentialProviderProvider.kt
@@ -0,0 +1,13 @@
+package com.facucastro.focusguard.providers.domain.auth
+
+import com.facucastro.focusguard.domain.auth.GoogleCredentialProvider
+import io.mockk.coEvery
+import io.mockk.mockk
+
+fun providesMockGoogleCredentialProvider(
+    idTokenResult: Result<String> = Result.success("mock_id_token")
+): GoogleCredentialProvider {
+    return mockk<GoogleCredentialProvider> {
+        coEvery { getGoogleIdToken(any()) } returns idTokenResult
+    }
+}
