a2a01426be1faa0c1bd7b0039eac9131ff3ca50e
Facundo Castro <facu073@gmail.com>
2026-03-24 12:17:54 -0300

    feat: initialize backend structure with Gradle and Spring Boot setup

---
+++ backend/src/test/kotlin/com/facucastro/focusguard/FocusguardApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.facucastro.focusguard
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FocusguardApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
