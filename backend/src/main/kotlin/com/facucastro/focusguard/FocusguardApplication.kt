cb63855bdfc4c8981f88cff681de577769c41f22
Facundo Castro <facu073@gmail.com>
2026-03-24 13:51:49 -0300

    Base Backend (Kotlin + Spring) (#8)
    * feat: moving android project to /android/
    
    * fix: update paths in unit tests workflow for Android project structure
    
    * feat: initialize backend structure with Gradle and Spring Boot setup
    
    * chore: update .gitignore to include local.properties and remove local.properties file

---
+++ backend/src/main/kotlin/com/facucastro/focusguard/FocusguardApplication.kt
@@ -0,0 +1,11 @@
+package com.facucastro.focusguard
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FocusguardApplication
+
+fun main(args: Array<String>) {
+	runApplication<FocusguardApplication>(*args)
+}
