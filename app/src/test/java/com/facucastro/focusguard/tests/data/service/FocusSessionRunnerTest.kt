39011937f1250ea248002b30b7582abde9563b4e
Facundo Castro <facu073@gmail.com>
2026-04-17 11:58:52 -0300

    test: add teardown method to FocusSessionRunnerTest and CompositeDistractionMonitorTest

--- app/src/test/java/com/facucastro/focusguard/tests/data/service/FocusSessionRunnerTest.kt
+++ app/src/test/java/com/facucastro/focusguard/tests/data/service/FocusSessionRunnerTest.kt
@@ -17,6 +17,7 @@ import kotlinx.coroutines.test.runCurrent
 import kotlinx.coroutines.test.runTest
 import org.junit.Assert.assertEquals
 import org.junit.Assert.assertNull
+import org.junit.After
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
--- app/src/test/java/com/facucastro/focusguard/tests/domain/sensor/CompositeDistractionMonitorTest.kt
+++ app/src/test/java/com/facucastro/focusguard/tests/domain/sensor/CompositeDistractionMonitorTest.kt
@@ -15,6 +15,7 @@ import kotlinx.coroutines.test.advanceUntilIdle
 import kotlinx.coroutines.test.runTest
 import org.junit.Assert.assertEquals
 import org.junit.Assert.assertTrue
+import org.junit.After
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
