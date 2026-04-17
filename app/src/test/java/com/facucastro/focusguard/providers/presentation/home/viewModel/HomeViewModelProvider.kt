f1f078677a4c1f069918c92505da49c524afae17
Facundo Castro <facu073@gmail.com>
2026-04-17 12:08:13 -0300

    feat: simplify HomeViewModelProvider and update HomeViewModelTest for microphone permission handling

--- app/src/test/java/com/facucastro/focusguard/providers/presentation/home/viewModel/HomeViewModelProvider.kt
+++ app/src/test/java/com/facucastro/focusguard/providers/presentation/home/viewModel/HomeViewModelProvider.kt
@@ -1,18 +1,9 @@
 package com.facucastro.focusguard.providers.presentation.home.viewModel
 
-import com.facucastro.focusguard.domain.session.FocusSessionState
 import com.facucastro.focusguard.presentation.home.viewModel.HomeViewModel
 import com.facucastro.focusguard.providers.domain.session.FakeFocusSessionController
 import com.facucastro.focusguard.providers.domain.session.providesFakeFocusSessionController
 
 fun providesHomeViewModel(
     sessionController: FakeFocusSessionController = providesFakeFocusSessionController(),
-    initialState: FocusSessionState = FocusSessionState(),
-): HomeViewModel {
-    val controller = if (sessionController.mutableState.value == FocusSessionState()) {
-        FakeFocusSessionController(initialState)
-    } else {
-        sessionController
-    }
-    return HomeViewModel(sessionController = controller)
-}
+): HomeViewModel = HomeViewModel(sessionController = sessionController)
