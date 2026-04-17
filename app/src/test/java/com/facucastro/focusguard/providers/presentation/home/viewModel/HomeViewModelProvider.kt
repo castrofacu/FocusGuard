9ec4a53c269b87477a281b4c2b8558326fc869e9
Facundo Castro <facu073@gmail.com>
2026-04-16 21:02:38 -0300

    feat: add FakeFocusSessionController and update HomeViewModelProvider to use it for session management

--- app/src/test/java/com/facucastro/focusguard/providers/presentation/home/viewModel/HomeViewModelProvider.kt
+++ app/src/test/java/com/facucastro/focusguard/providers/presentation/home/viewModel/HomeViewModelProvider.kt
@@ -1,47 +1,18 @@
 package com.facucastro.focusguard.providers.presentation.home.viewModel
 
-import com.facucastro.focusguard.domain.model.DistractionEvent
-import com.facucastro.focusguard.domain.notification.DistractionNotifier
-import com.facucastro.focusguard.domain.sensor.DistractionMonitor
-import com.facucastro.focusguard.domain.time.TimeProvider
-import com.facucastro.focusguard.domain.timer.FocusSessionTimerFactory
-import com.facucastro.focusguard.domain.usecase.FocusTimerUseCase
-import com.facucastro.focusguard.domain.usecase.ObserveDistractionsUseCase
-import com.facucastro.focusguard.domain.usecase.StartFocusSessionUseCase
-import com.facucastro.focusguard.domain.usecase.StopFocusSessionUseCase
+import com.facucastro.focusguard.domain.session.FocusSessionState
 import com.facucastro.focusguard.presentation.home.viewModel.HomeViewModel
-import com.facucastro.focusguard.providers.domain.repository.providesMockFocusRepository
-import com.facucastro.focusguard.providers.domain.sensor.providesFakeDistractionMonitor
-import com.facucastro.focusguard.providers.domain.time.StepTimeProvider
-import com.facucastro.focusguard.providers.domain.usecase.providesFakeFocusTimerUseCase
-import io.mockk.every
-import io.mockk.just
-import io.mockk.mockk
-import io.mockk.runs
-import kotlinx.coroutines.flow.emptyFlow
+import com.facucastro.focusguard.providers.domain.session.FakeFocusSessionController
+import com.facucastro.focusguard.providers.domain.session.providesFakeFocusSessionController
 
 fun providesHomeViewModel(
-    distractionMonitor: DistractionMonitor = providesFakeDistractionMonitor(),
-    stopResult: Result<Unit> = Result.success(Unit),
-    timeProvider: TimeProvider = StepTimeProvider(),
-    focusTimerUseCase: FocusTimerUseCase = providesFakeFocusTimerUseCase(
-        invokeResult = emptyFlow()
-    ),
+    sessionController: FakeFocusSessionController = providesFakeFocusSessionController(),
+    initialState: FocusSessionState = FocusSessionState(),
 ): HomeViewModel {
-    val focusRepository = providesMockFocusRepository(saveResult = stopResult)
-    val distractionNotifier = mockk<DistractionNotifier> {
-        every { notifyDistraction(any<DistractionEvent>()) } just runs
+    val controller = if (sessionController.mutableState.value == FocusSessionState()) {
+        FakeFocusSessionController(initialState)
+    } else {
+        sessionController
     }
-
-    return HomeViewModel(
-        startFocusSessionUseCase = StartFocusSessionUseCase(timeProvider),
-        stopFocusSessionUseCase = StopFocusSessionUseCase(focusRepository, timeProvider),
-        focusTimerUseCase = focusTimerUseCase,
-        observeDistractionsUseCase = ObserveDistractionsUseCase(
-            distractionMonitor = distractionMonitor,
-            distractionNotifier = distractionNotifier,
-            timeProvider = timeProvider,
-        ),
-        timerFactory = FocusSessionTimerFactory(timeProvider),
-    )
+    return HomeViewModel(sessionController = controller)
 }
