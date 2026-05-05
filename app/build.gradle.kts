95df2048de6db8772555c82e1a533d81c0bd1ea9
Facundo Castro <facu073@gmail.com>
2026-05-05 00:11:29 -0300

    feat: Add Firebase Remote Config dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -113,6 +113,7 @@ dependencies {
     implementation(platform(libs.firebase.bom))
     implementation(libs.firebase.crashlytics)
     implementation(libs.firebase.auth)
+    implementation(libs.firebase.config)
 
     implementation(libs.androidx.credentials)
     implementation(libs.androidx.credentials.play.services.auth)
